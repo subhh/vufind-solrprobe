4c336c9c4cd3d6ba78e45930ec8355e59e0d70ec
David Maus <dmaus@dmaus.name>
2022-12-31 12:34:48 +0100

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables         -*- no-byte-compile: t; -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((project-name . "SolrProbe")
+         (project-owner . "Staats- und Universitätsbibliothek Hamburg)))
